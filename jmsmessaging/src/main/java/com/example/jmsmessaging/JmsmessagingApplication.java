705bd571e5eb10dd4ed03613dad0215ccabd98c0
pooyafils <pooyafils@gmail.com>
2021-02-27 02:34:35 +0330

    adding jms config file

---
+++ jmsmessaging/src/main/java/com/example/jmsmessaging/JmsmessagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmsmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsmessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsmessagingApplication.class, args);
+	}
+
+}
