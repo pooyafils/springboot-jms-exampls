79854fe639bd3dcc9fb65326a1284f4f57c7d901
pooyafils <pooyafils@gmail.com>
2021-11-29 01:51:59 +0330

    editing controller

--- kafkaproducer/src/main/java/com/example/kafkaproducer/controller/UserController.java
+++ kafkaproducer/src/main/java/com/example/kafkaproducer/controller/UserController.java
@@ -1,24 +1,25 @@
 package com.example.kafkaproducer.controller;
 
 import com.example.kafkaproducer.model.User;
+import com.example.kafkaproducer.repository.UserRepository;
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/kafkaproducer")
 public class UserController {
     @Autowired
 KafkaTemplate<String, User> kafkaTemplate;
+    @Autowired
+    UserRepository userRepository;
     private final String TOPIC="Kafka_Example";
-    @GetMapping("/{message}")
-    public String sendMessage(@PathVariable("message") final String message){
-        kafkaTemplate.send(TOPIC,new User(1,"java","kafka"));
+    @GetMapping
+    public String sendMessage(@RequestBody User user){
+        userRepository.save(user);
+        kafkaTemplate.send(TOPIC,user);
         return "message has been send";
     }
 }
